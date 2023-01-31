6cec333719b6da907a97af60a2154d90dde6afa6
Nathaniel Truitt <nathantruitt2@gmail.com>
2023-01-31 12:41:40 -0600

    adding posts model

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
+  include SessionsHelper
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
