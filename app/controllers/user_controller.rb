c3ff3dc3e73bc73a4a0083468a18695cebe85988
Nathaniel Truitt <nathantruitt2@gmail.com>
2023-01-30 20:10:53 -0600

    adding devise

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,14 @@
+class UserController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      respond_to do |format|
+        format.html { redirect_to }
+      end
+    end
+  end
+end
